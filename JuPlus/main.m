2351d3742533c025ae32f55c64add23266f69702
wenbo zhan <wbzhan@atzuche.com>
2015-06-11 13:49:39 +0800

    Initial Commit

---
+++ JuPlus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JuPlus
+//
+//  Created by 詹文豹 on 15/6/11.
+//  Copyright (c) 2015年 居+. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
