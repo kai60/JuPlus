446b04f06e495c57747de962287cd4876b7f9fd9
zhanwenbaoit@126.com <794205256@qq.com>
2015-06-29 10:25:25 +0800

    workspace commit

---
+++ JuPlus/ViewControllers/Detail/PlaceOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaceOrderViewController.h
+//  JuPlus
+//
+//  Created by admin on 15/6/29.
+//  Copyright (c) 2015年 居+. All rights reserved.
+//
+
+#import "BaseViewController.h"
+//下单界面
+@interface PlaceOrderViewController : BaseViewController
+
+@end
--- JuPlus/ViewControllers/HomePage/JuPlusDTO/BaseDTO.h
+++ JuPlus/ViewControllers/HomePage/JuPlusDTO/BaseDTO.h
@@ -9,5 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface BaseDTO : NSObject
--(void)loadDTO:(NSDictionary *)dict
+-(void)loadDTO:(NSDictionary *)dict;
+
+
 @end
